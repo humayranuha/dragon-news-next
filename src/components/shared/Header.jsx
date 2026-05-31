ff091c5c3c8c17c64ef9231a1b4868da2e36f948
humayranuha <nuhahumayra24@gmail.com>
2026-05-31 20:18:44 +0600

    feat: Header designed

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from "@/assets/logo.png";
+import Image from 'next/image';
+import { format, compareAsc } from "date-fns";
+
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='grid justify-center items-center text-center space-y-2 py-5'>
+            <Image src={logo} width={300} height={200} alt='logo' />
+            <p className='text-gray-500 text-md'>Journalism without Fear and Favor</p>
+            <p>{format(new Date(), "EEEE, MMM dd, yyyy")}</p>
         </div>
     );
 };
