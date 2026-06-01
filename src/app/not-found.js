938fc87e29d786cb56e0680668deafccb84e5667
humayranuha <nuhahumayra24@gmail.com>
2026-06-01 11:18:58 +0600

    feat: Home button added to the not-found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,21 @@
 import LoadingAnimation from '@/components/LoadingAnimation';
 import NotFoundAnimation from '@/components/NotFoundAnimation';
+import { Link } from '@heroui/react';
 
 export default function NotFound() {
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            {/* <LoadingAnimation /> */}
-            <NotFoundAnimation />
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="flex items-center justify-center">
+                {/* <LoadingAnimation /> */}
+                <NotFoundAnimation />
+            </div>
+            <div className="mt-8">
+                <Link href="/">
+                    <button className='btn bg-red-500 text-white px-6 py-2 rounded hover:bg-red-600 transition-colors'>
+                        Go Home
+                    </button>
+                </Link>
+            </div>
         </div>
     );
 }
