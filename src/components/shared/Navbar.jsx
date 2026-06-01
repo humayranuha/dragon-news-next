23c1bcc2430d3e765d181803867ca1baebd08804
humayranuha <nuhahumayra24@gmail.com>
2026-06-01 11:55:15 +0600

    feat: active status implemented through customized NavLink

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,15 +2,16 @@ import Link from 'next/link';
 import React from 'react';
 import userAvatar from '@/assets/user.png';
 import Image from 'next/image';
+import NavLink from './NavLink';
 
 const Navbar = () => {
     return (
         <div className='flex justify-between items-center container mx-auto my-3'>
             <div></div>
             <ul className='flex justify-between items-center gap-3'>
-                <li><Link href={'/'} className='hover:text-red-500'>Home</Link></li>
-                <li><Link href={'/about'} className='hover:text-red-500'>About</Link></li>
-                <li><Link href={'/career'} className='hover:text-red-500'>Career</Link></li>
+                <li><NavLink href={'/'} className='hover:text-red-500'>Home</NavLink></li>
+                <li><NavLink href={'/about'} className='hover:text-red-500'>About</NavLink></li>
+                <li><NavLink href={'/career'} className='hover:text-red-500'>Career</NavLink></li>
             </ul>
             <div className='flex justify-between items-center gap-1'>
                 <Image src={userAvatar} width={40} height={40} alt='user avatar' />
