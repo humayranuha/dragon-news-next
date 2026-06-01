5ae6bfcec66cf7c969a86b22fbf5be2ecedb36eb
humayranuha <nuhahumayra24@gmail.com>
2026-06-01 11:12:11 +0600

    style: hover feature added to navbar links

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
         <div className='flex justify-between items-center container mx-auto my-3'>
             <div></div>
             <ul className='flex justify-between items-center gap-3'>
-                <li><Link href={'/'}>Home</Link></li>
-                <li><Link href={'/about'}>About</Link></li>
-                <li><Link href={'/career'}>Career</Link></li>
+                <li><Link href={'/'} className='hover:text-red-500'>Home</Link></li>
+                <li><Link href={'/about'} className='hover:text-red-500'>About</Link></li>
+                <li><Link href={'/career'} className='hover:text-red-500'>Career</Link></li>
             </ul>
             <div className='flex justify-between items-center gap-1'>
                 <Image src={userAvatar} width={40} height={40} alt='user avatar' />
