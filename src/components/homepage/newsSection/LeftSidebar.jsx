4d366339ba27a7c44a060b9ce70d112ff41069c1
humayranuha <nuhahumayra24@gmail.com>
2026-06-01 14:18:04 +0600

    feat: categories are made dynamic

--- src/components/homepage/newsSection/LeftSidebar.jsx
+++ src/components/homepage/newsSection/LeftSidebar.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const LeftSidebar = ({categories, activeId}) => {
@@ -6,7 +7,7 @@ const LeftSidebar = ({categories, activeId}) => {
             <h1 className="text-xl font-bold text-center p-2">All Categories</h1>
             <ul className="flex flex-col gap-3">
                 {categories.news_category.map(category => {
-                    return <li key={category.category_id} className={`${activeId === category.category_id && "bg-slate-100 "} text-sm rounded-md p-2 text-center `}>{category.category_name}</li>
+                    return <li key={category.category_id} className={`${activeId === category.category_id && "bg-slate-100 "} text-sm rounded-md p-2 text-center `}><Link href={`/category/${category.category_id}`}>{category.category_name}</Link></li>
                 })}
             </ul>
         </div>
