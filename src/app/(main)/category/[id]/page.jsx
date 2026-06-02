20b0cb1b10afdd3f99b258918fbccf3a71220366
humayranuha <nuhahumayra24@gmail.com>
2026-06-02 11:30:47 +0600

    feat: category-wise news presentation

---
+++ src/app/(main)/category/[id]/page.jsx
@@ -0,0 +1,45 @@
+import LeftSidebar from '@/components/homepage/newsSection/LeftSidebar';
+import RightSidebar from '@/components/homepage/newsSection/RightSidebar';
+import React from 'react';
+
+async function getCategories() {
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await res.json();
+    return data.data;
+}
+
+async function getNewsByCategoryId(category_id) {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
+    const data = await res.json();
+    return data.data;
+}
+
+const CategoryNewsPage = async ({params}) => {
+    const {id} = await params;
+    console.log(id);
+
+    const categories = await getCategories();
+    // console.log(categories.news_category);
+
+    const news = await getNewsByCategoryId(id);
+    // console.log(news);
+
+    return (
+        <div className="grid grid-cols-12 gap-3 container mx-auto my-11">
+            <div className="text-black text-2xl col-span-3 p-2">
+                <LeftSidebar categories={categories} activeId="01" />
+            </div>
+            <div className="text-black text-2xl col-span-6 space-y-2">
+                All News
+                {news.map(n => {
+                    return <div key={n._id} className="p-5 border rounded-md">{n.title}</div>
+                })}
+            </div>
+            <div className="text-black col-span-3 p-3">
+                <RightSidebar />
+            </div>
+        </div>
+    );
+};
+
+export default CategoryNewsPage;
--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -3,24 +3,11 @@ import RightSidebar from "@/components/homepage/newsSection/RightSidebar";
 import LoadingAnimation from "@/components/LoadingAnimation";
 import Image from "next/image";
 
-async function getCategories() {
-  const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
-  const data = await res.json();
-  return data.data;
-}
+
 
 export default async function Home() {
-  const categories = await getCategories();
-  console.log(categories.news_category);
+  
   return (
-    <div className="grid grid-cols-12 gap-3 container mx-auto my-11">
-      <div className="text-black text-2xl col-span-3 p-2">
-        <LeftSidebar categories={categories} activeId="01"/>
-      </div>
-      <div className="text-white text-2xl bg-red-800 col-span-6">All News</div>
-      <div className="text-black col-span-3 p-3">
-        <RightSidebar />
-      </div>
-    </div>
+    <div></div>
   );
 }
