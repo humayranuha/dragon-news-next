8b91e2c35c83ae2490e02d51c37a3b608bebf156
humayranuha <nuhahumayra24@gmail.com>
2026-06-03 01:18:11 +0600

    feat: navbar updated with sign in info

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,10 +1,15 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
+import { UserButton, useAuth, SignInButton } from '@clerk/nextjs';
 import userAvatar from '@/assets/user.png';
 import Image from 'next/image';
 import NavLink from './NavLink';
 
 const Navbar = () => {
+    const { isSignedIn } = useAuth();
+
     return (
         <div className='flex justify-between items-center container mx-auto my-3'>
             <div></div>
@@ -13,9 +18,28 @@ const Navbar = () => {
                 <li><NavLink href={'/about'} className='hover:text-red-500'>About</NavLink></li>
                 <li><NavLink href={'/career'} className='hover:text-red-500'>Career</NavLink></li>
             </ul>
-            <div className='flex justify-between items-center gap-1'>
-                <Image src={userAvatar} width={40} height={40} alt='user avatar' />
-                <button className='btn bg-red-800 text-white'><Link href={'/login'}>Login</Link></button>
+            <div className='flex justify-between items-center gap-3'>
+                {isSignedIn ? (
+                    // Show UserButton when logged in
+                    <UserButton 
+                        afterSignOutUrl="/category/01"
+                        appearance={{
+                            elements: {
+                                userButtonAvatarBox: "w-10 h-10",
+                            }
+                        }}
+                    />
+                ) : (
+                    // Show Login button when logged out
+                    <>
+                        <Image src={userAvatar} width={40} height={40} alt='user avatar' />
+                        <SignInButton mode="redirect">
+                            <button className='btn bg-red-800 text-white hover:bg-red-700'>
+                                Login
+                            </button>
+                        </SignInButton>
+                    </>
+                )}
             </div>
         </div>
     );
