f5d3e85dafe0677a86a62e43240f5d2db68c542a
humayranuha <nuhahumayra24@gmail.com>
2026-06-02 21:55:28 +0600

    feat: clerk authentication enabled

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,10 @@ import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/shared/Header";
 import Navbar from "@/components/shared/Navbar";
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
+import LoginPage from "./(auth)/login/page";
+import RegisterPage from "./(auth)/register/page";
+import { Show, SignInButton, SignUpButton } from '@clerk/nextjs';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,13 +30,24 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className={`${poppins.className} min-h-full flex flex-col`}>
-        
-        {children}
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ClerkProvider>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
